8bbac40347b583fa02a857aa65bd160b464f9ea0
Jared <jcknueven@gmail.com>
2016-06-20 14:29:55 -0400

    can search api, moving to click func

--- js/main.js
+++ js/main.js
@@ -5,19 +5,20 @@ $( document ).ready(function() {
 $("form").on("submit",function(e){
 	e.preventDefault();
 
-	var artist = $(".artistSearch").input.val();
-
-	var data = {
-		artist,
-	};
+	var artist = $(".artistSearch").val();
 
 	$.ajax({
-	url: "http://api.soundcloud.com/tracks?client_id=03e4633e2d85874a921380e47cac705d&", 
-	data: data,
-	type:"get",
-	success: function(result)
+	url: "http://api.soundcloud.com/tracks?client_id=03e4633e2d85874a921380e47cac705d&q="+artist,
+	type:"GET",
+	success: function(songs)
 	{
-		$('audio').append("<source src='")
+		songs.forEach(function(song)
+		{
+			$('.Results').append("<div class='resultName'>"+song.title+"</div><div class='col-lg-3 resultArt'><img src='"+song.artwork_url+"'></div>");
+		})
+		$("img").on('click', function(song){
+			$("audio").append("<source src='")
+		})
 	}
 });
 })
