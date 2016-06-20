010ce6fdf32eb43642ed91f834498f4e0b45d0f6
Jared <jcknueven@gmail.com>
2016-06-20 15:54:42 -0400

    linked id to audio player and added reset to search

--- js/main.js
+++ js/main.js
@@ -2,28 +2,35 @@ $( document ).ready(function() {
   // Handler for .ready() called.
 
 
-$("form").on("submit",function(e){
-	e.preventDefault();
+	$("form").on("submit",function(e){
+		e.preventDefault();
 
-	var artist = $(".artistSearch").val();
+		var artist = $(".artistSearch").val();
 
-	$.ajax({
-	url: "http://api.soundcloud.com/tracks?client_id=03e4633e2d85874a921380e47cac705d&q="+artist,
-	type:"GET",
-	success: function(songs)
-	{
-		songs.forEach(function(song)
+		$.ajax({
+		url: "http://api.soundcloud.com/tracks?client_id=03e4633e2d85874a921380e47cac705d&q="+artist,
+		type:"GET",
+		success: function(songs)
 		{
-			$('.Results').append("<div class='resultName'>"+song.title+"</div><div class='col-lg-3 resultArt'><img src='"+song.artwork_url+"'></div>");
-		})
-		$("img").on('click', function(song){
-			$("audio").append("<source src='")
-		})
-	}
-});
-})
+			songs.forEach(function(song)
+			{
+				$('.Results').append("<div class='resultName'><p>"+song.title+"</p></div><div class='resultArt'><img data-title='"+song.title+"' data-soundcloud='"+song.id+"' src='"+song.artwork_url+"'></div>");
 
-
-
-
+			});
+			$(".artistSearch").val("");
+			$("form").on("submit", function(){
+				$('.Results').html("");
+			})
+		}
+		});
+	})
+	$("body").on("click", '.resultArt img',function(e)
+	{
+		var sc = $(this).data('soundcloud');
+		$( "audio" ).attr( "src", function(){
+  		return "http://api.soundcloud.com/tracks/"+sc+"/stream?client_id=03e4633e2d85874a921380e47cac705d";
+		});
+		var sc1 = $(this).data('title');
+		$( ".playing p").html(sc1);
+	});
 });
